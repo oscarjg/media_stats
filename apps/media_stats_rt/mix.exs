2d5efdcee45f67cb0eac4712432c33ea777cd520
Oscar Jimenez <oscarjg19.developer@gmail.com>
2019-07-11 12:57:10 +0200

    reset

--- apps/media_stats_rt/mix.exs
+++ apps/media_stats_rt/mix.exs
@@ -4,7 +4,7 @@ defmodule MediaStatsRT.MixProject do
   def project do
     [
       app: :media_stats_rt,
-      version: "0.0.3",
+      version: "0.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
